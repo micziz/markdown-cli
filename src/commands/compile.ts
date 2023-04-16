bee1adee825371abf1e7074f19cf42c540c99412
micziz <miczicontent@gmail.com>
2023-04-16 11:01:26 +0200

    Add fm support

--- src/commands/compile.ts
+++ src/commands/compile.ts
@@ -2,12 +2,32 @@ import { readFile, writeFile } from "fs/promises";
 import { join } from "path";
 import { cwd } from "process";
 import { micromark } from 'micromark'
+import matter from 'gray-matter'
+
+function handleContent(params: {compileEdContent: string, title: string, description: string}){
+    return `
+<html>
+    <head>
+        <title>${params.title}</title>
+        <meta name="description" content="${params.description}">
+    </head>
+    <body>
+        ${params.compileEdContent}
+    </body>
+</html>
+`
+}
 
 export async function compile(params: { in: string, out?: string }){
     try {
-        const contentToCompile = await readFile(join(cwd(), params.in))
-        const content = micromark(contentToCompile)
+        const readWithFrontMatter = await readFile(join(cwd(), params.in))
+        const parsedFile = matter(readWithFrontMatter)
+        const frontMatter = parsedFile.data
+        const content = micromark(parsedFile.content)
+        
         try {
+            const title: string = (typeof frontMatter.title !== "undefined") ? frontMatter.title : "Default title"
+            const description: string = (typeof frontMatter.description !== "undefined") ? frontMatter.description : "Default description"
             let path: string;
             if (params.out !== undefined){
                 console.log(params.out)
@@ -16,7 +36,7 @@ export async function compile(params: { in: string, out?: string }){
                 path = "./index.html"
             }
             console.log(path)
-            await writeFile(path, content)
+            await writeFile(path, handleContent({compileEdContent: content, title: title, description: description}))
         } catch (e){
             console.log("There was an error writing the file!")
         }
